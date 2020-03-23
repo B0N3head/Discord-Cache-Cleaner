9f7d27d12dbe7dc3cebd024685220dc9ef8dca97
B0N3head <47313866+B0N3head@users.noreply.github.com>
2020-03-23 13:32:05 +1000

    shrunk dcc

--- DCC.bat
+++ DCC.bat
@@ -1,23 +1,24 @@
 @echo off
-title Discord Cache Cleaner
 set u=%userprofile%
-if exist %u%\DCC\trid.exe (goto setupdone)
-echo Just setting up dependencies
+set t=trid_w32.zip
+set r=triddefs.zip
+set w=https://mark0.net/download/
+set c=%APPDATA%\discord\Cache
+if exist %u%\DCC\trid.exe (goto d)
 cd %u%
 md DCC
 cd %u%\DCC
-curl -o trid_w32.zip https://mark0.net/download/trid_w32.zip
-curl -o triddefs.zip https://mark0.net/download/triddefs.zip
-powershell Expand-Archive -Force %u%\DCC\trid_w32.zip %u%\DCC
-powershell Expand-Archive -Force %u%\DCC\triddefs.zip %u%\DCC
-del %u%\DCC\trid_w32.zip
-del %u%\DCC\triddefs.zip
-cls
-:setupdone
-cd %APPDATA%\discord\Cache
+curl -o %t% %w%%t%
+curl -o %r% %w%%r%
+powershell Expand-Archive -Force %u%\DCC\%t% %u%\DCC
+powershell Expand-Archive -Force %u%\DCC\%r% %u%\DCC
+del %u%\DCC\%t%
+del %u%\DCC\%r%
+:d
+cd %c%
+echo Working...
 start.
-%u%\DCC\trid %APPDATA%\discord\Cache\* -ae >nul
-echo Continuing will delete cache, feel free to close if otherwise
-pause
-echo y | del %APPDATA%\discord\Cache\*.*
-exit
+%u%\DCC\trid %c%\* -ae >nul
+echo Press any button to delete cache
+pause >nul
+echo y | del %c%\*.*

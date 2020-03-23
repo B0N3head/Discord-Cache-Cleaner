a81c8c2c589ca45cb04b0fa90d03a00f1b06ad08
B0N3head <47313866+B0N3head@users.noreply.github.com>
2020-03-23 13:33:32 +1000

    Update and rename DCC-UI.bat to DCC-With-Text.bat

--- DCC-With-Text.bat
+++ DCC-With-Text.bat
@@ -1,27 +1,26 @@
 @echo off
 title Discord Cache Cleaner
-set u=%userprofile%
-if exist %u%\DCC\trid.exe (goto setupdone)
+if exist %userprofile%\DCC\trid.exe (goto setupdone)
 echo Just setting up dependencies
-cd %u%
+cd %userprofile%
 md DCC
-cd %u%\DCC
+cd %userprofile%\DCC
 echo Downloading Trid
 curl -o trid_w32.zip https://mark0.net/download/trid_w32.zip
 echo Downloading Trid config
 curl -o triddefs.zip https://mark0.net/download/triddefs.zip
 echo Extracting Trid
-powershell Expand-Archive -Force %u%\DCC\trid_w32.zip %u%\DCC
-powershell Expand-Archive -Force %u%\DCC\triddefs.zip %u%\DCC
-del %u%\DCC\trid_w32.zip
-del %u%\DCC\triddefs.zip
+powershell Expand-Archive -Force %userprofile%\DCC\trid_w32.zip %userprofile%\DCC
+powershell Expand-Archive -Force %userprofile%\DCC\triddefs.zip %userprofile%\DCC
+del %userprofile%\DCC\trid_w32.zip
+del %userprofile%\DCC\triddefs.zip
 cls
 :setupdone
 cd %APPDATA%\discord\Cache
 start.
 echo Finding file types and renaming them...
 echo This could take some time but feel free to scroll through
-%u%\DCC\trid %APPDATA%\discord\Cache\* -ae >nul
+%userprofile%\DCC\trid %APPDATA%\discord\Cache\* -ae >nul
 cls
 echo Continuing from this point will delete the cache
 echo Feel free to close if you feel otherwise

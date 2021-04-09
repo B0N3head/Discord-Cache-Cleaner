b25fba0436a2faee8fd9a0558f3840f899e5f4d9
Bonehead <47313866+B0N3head@users.noreply.github.com>
2021-04-09 23:06:33 +1000

    Consistent Path

--- DCC-Full.bat
+++ DCC-Full.bat
@@ -1,26 +1,26 @@
 @echo off
 title Discord Cache Cleaner
-if exist %userprofile%\DCC\trid.exe (goto d)
+if exist %AppData%\DCC\trid.exe (goto d)
 echo.
 echo Downloading dependencies
-cd %userprofile%
+cd /D %AppData%
 md DCC
-cd %userprofile%\DCC
+cd %AppData%\DCC
 curl -o trid_w32.zip https://mark0.net/download/trid_w32.zip
 curl -o triddefs.zip https://mark0.net/download/triddefs.zip
 echo Extracting dependencies
-powershell Expand-Archive -Force %userprofile%\DCC\trid_w32.zip %userprofile%\DCC
-powershell Expand-Archive -Force %userprofile%\DCC\triddefs.zip %userprofile%\DCC
+powershell Expand-Archive -Force %AppData%\DCC\trid_w32.zip %AppData%\DCC
+powershell Expand-Archive -Force %AppData%\DCC\triddefs.zip %AppData%\DCC
 echo Cleaning trash
-del %userprofile%\DCC\trid_w32.zip
-del %userprofile%\DCC\triddefs.zip
+del %AppData%\DCC\trid_w32.zip
+del %AppData%\DCC\triddefs.zip
 :d
 cd %APPDATA%\discord\Cache
 cls
 echo.
-echo Wroking on renaming apropiate file types
-start.
-%userprofile%\DCC\trid %APPDATA%\discord\Cache\* -ae >nul
+echo Converting Whole Cache to Images
+start %APPDATA%\discord\Cache
+%AppData%\DCC\trid %APPDATA%\discord\Cache\* -ae >nul
 cls
 echo.
 echo Press any button to delete cache, otherwise feel free to close this window

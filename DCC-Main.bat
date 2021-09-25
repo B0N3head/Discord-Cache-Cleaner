dfdff84202f9bc890c82d6a0c4461d106a9487b7
Bonehead <47313866+B0N3head@users.noreply.github.com>
2021-09-25 12:17:43 +1000

    Fixed issue with usernames

--- DCC-Main.bat
+++ DCC-Main.bat
@@ -4,24 +4,24 @@ set t=trid_w32.zip
 set r=triddefs.zip
 set w=https://mark0.net/download/
 set c=%APPDATA%\discord\Cache
-if exist %u%\DCC\trid.exe (goto d)
+if exist "%u%\DCC\trid.exe" (goto d)
 echo Downloading dependencies
 cd %u%
 md DCC
-cd %u%\DCC
+cd "%u%\DCC"
 curl -o %t% %w%%t%
 curl -o %r% %w%%r%
 powershell Expand-Archive -Force %u%\DCC\%t% %u%\DCC
 powershell Expand-Archive -Force %u%\DCC\%r% %u%\DCC
-del %u%\DCC\%t%
-del %u%\DCC\%r%
+del "%u%\DCC\%t%"
+del "%u%\DCC\%r%"
 :d
-cd %c%
+cd "%c%"
 cls
 echo Converting...
-start %c%
-%u%\DCC\trid %c%\* -ae >nul
+start "" "%c%"
+"%u%\DCC\trid" "%c%\*" -ae >nul
 cls
 echo Press any button to delete cache
 pause
-echo y | del %c%\*.*
+echo y | del "%c%\*.*"
